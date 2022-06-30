779fa3fab3404a0772e2851f70d8d3837b663b67
OhSeonMu <tjsan0122@gmail.com>
2022-06-30 17:31:41 +0900

    commit msg

--- tab/app/src/main/java/com/example/tab/MainActivity.java
+++ tab/app/src/main/java/com/example/tab/MainActivity.java
@@ -4,16 +4,34 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.LinearLayout;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import com.google.android.material.tabs.TabLayout;
 
 public class MainActivity extends AppCompatActivity {
 
+    static final String[] LIST_MENU = {"01065903013", "01011111234", "01056478839"};
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, LIST_MENU);
+
+        ListView listView = (ListView) findViewById(R.id.listView);
+        listView.setAdapter(adapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView parent, View v, int position, long id) {
+                String strText = (String) parent.getItemAtPosition(position);
+            }
+        });
 
         TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs) ;
         tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
@@ -37,25 +55,29 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void changeView(int index) {
+        LinearLayout linearLayout1 = (LinearLayout) findViewById(R.id.LinearLayout1) ;
         TextView textView1 = (TextView) findViewById(R.id.text1) ;
         TextView textView2 = (TextView) findViewById(R.id.text2) ;
         TextView textView3 = (TextView) findViewById(R.id.text3) ;
 
         switch (index) {
             case 0 :
+                linearLayout1.setVisibility(View.VISIBLE);
                 textView1.setVisibility(View.INVISIBLE) ;
-                textView2.setVisibility(View.INVISIBLE) ;
-                textView3.setVisibility(View.VISIBLE) ;
-                break ;
-            case 1 :
-                textView1.setVisibility(View.VISIBLE) ;
                 textView2.setVisibility(View.INVISIBLE) ;
                 textView3.setVisibility(View.INVISIBLE) ;
                 break ;
-            case 2 :
+            case 1 :
+                linearLayout1.setVisibility(View.INVISIBLE);
                 textView1.setVisibility(View.INVISIBLE) ;
                 textView2.setVisibility(View.VISIBLE) ;
                 textView3.setVisibility(View.INVISIBLE) ;
+                break ;
+            case 2 :
+                linearLayout1.setVisibility(View.INVISIBLE);
+                textView1.setVisibility(View.INVISIBLE) ;
+                textView2.setVisibility(View.INVISIBLE) ;
+                textView3.setVisibility(View.VISIBLE) ;
                 break ;
 
         }
