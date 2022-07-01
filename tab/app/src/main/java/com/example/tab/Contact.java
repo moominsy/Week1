3059558e6db38d17dc800369c51c4e20bfa10b1c
OhSeonMu <tjsan0122@gmail.com>
2022-07-01 21:38:47 +0900

    add contact to tab

---
+++ tab/app/src/main/java/com/example/tab/Contact.java
@@ -0,0 +1,28 @@
+package com.example.tab;
+
+public class Contact {
+    private Long Photoid;
+    private String Phonenum;
+    private String Name;
+
+
+    public Long getPhotoid(){
+        return Photoid;
+    }
+    public String getPhonenum(){
+        return Phonenum;
+    }
+    public String getName(){
+        return Name;
+    }
+
+    public void setPhotoid(Long Photoid){
+        this.Photoid = Photoid;
+    }
+    public void setPhonenum(String Phonenum){
+        this.Phonenum = Phonenum;
+    }
+    public void setName(String Name){
+        this.Name = Name;
+    }
+}
--- tab/app/src/main/java/com/example/tab/MainActivity.java
+++ tab/app/src/main/java/com/example/tab/MainActivity.java
@@ -1,38 +1,75 @@
 package com.example.tab;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.annotation.TargetApi;
+import android.content.DialogInterface;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.ContactsContract;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.material.tabs.TabLayout;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
 
-    static final String[] LIST_MENU = {"01065903013", "01011111234", "01056478839"};
+    //TAB1//
+    Button btnShowContacts1;
+    Button btnShowContacts2;
+    int check = 0;
+    public static final int PERMISSIONS_REQUEST_READ_CONTACTS = 1;
+    ArrayList<String> numberlist = new ArrayList<>();
+    //TAB2//
+
+    //TAB3//
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, LIST_MENU);
+        //TAB1//
 
-        ListView listView = (ListView) findViewById(R.id.listView);
-        listView.setAdapter(adapter);
+        numberlist.add("01012345678");
+        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, numberlist) ;
 
-        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+        ListView listview = (ListView) findViewById(R.id.listView) ;
+        listview.setAdapter(adapter) ;
+        btnShowContacts1 = (Button) findViewById(R.id.btnShowContact1);
+
+        btnShowContacts1.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onItemClick(AdapterView parent, View v, int position, long id) {
-                String strText = (String) parent.getItemAtPosition(position);
+            public void onClick(View v) {
+                requestContactPermission();
+                ArrayList<Contact> contactlist = getContacts();
+                ArrayList<String> numberlist1 = getnumberlist(contactlist);
+                for(int i = 0; i <numberlist1.size(); i++){
+                    numberlist.add(numberlist1.get(i));
+                }
+                adapter.notifyDataSetChanged();
             }
         });
 
+        //TAB2//
+
+        //TAB3//
+
+        //TAB//
         TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs) ;
         tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
 
@@ -54,30 +91,116 @@ public class MainActivity extends AppCompatActivity {
         }) ;
     }
 
+    //TAB1//
+
+    private ArrayList<Contact> getContacts() {
+        //TODO get contacts code here
+        Toast.makeText(this, "Get contacts ....", Toast.LENGTH_LONG).show();
+        Uri uri = ContactsContract.CommonDataKinds.Phone.CONTENT_URI;
+        String[] projection = new String[] {
+                ContactsContract.CommonDataKinds.Phone.CONTACT_ID,
+                ContactsContract.CommonDataKinds.Phone.NUMBER, // 연락처
+                ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME }; // 연락처 이름
+
+        String[] selectionArgs = null;
+
+        String sortOrder = ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME + " COLLATE LOCALIZED ASC";
+
+        Cursor contactCursor = getContentResolver().query(uri, projection, null, selectionArgs, sortOrder);
+
+        ArrayList<Contact> contactlist = new ArrayList<Contact>();
+
+        if (contactCursor.moveToFirst()) {
+            do {
+                Contact acontact = new Contact();
+                acontact.setPhotoid(contactCursor.getLong(0));
+                acontact.setPhonenum(contactCursor.getString(1));
+                acontact.setName(contactCursor.getString(2));
+                contactlist.add(acontact);
+            } while (contactCursor.moveToNext());
+        }
+        return contactlist;
+    }
+
+    public void requestContactPermission() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.READ_CONTACTS) != PackageManager.PERMISSION_GRANTED) {
+                if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                        android.Manifest.permission.READ_CONTACTS)) {
+                    AlertDialog.Builder builder = new AlertDialog.Builder(this);
+                    builder.setTitle("Read Contacts permission");
+                    builder.setPositiveButton(android.R.string.ok, null);
+                    builder.setMessage("Please enable access to contacts.");
+                    builder.setOnDismissListener(new DialogInterface.OnDismissListener() {
+                        @TargetApi(Build.VERSION_CODES.M)
+                        @Override
+                        public void onDismiss(DialogInterface dialog) {
+                            requestPermissions(
+                                    new String[]
+                                            {android.Manifest.permission.READ_CONTACTS}
+                                    , PERMISSIONS_REQUEST_READ_CONTACTS);
+                        }
+                    });
+                    builder.show();
+                } else {
+                    ActivityCompat.requestPermissions(this,
+                            new String[]{android.Manifest.permission.READ_CONTACTS},
+                            PERMISSIONS_REQUEST_READ_CONTACTS);
+                }
+            } else {
+                getContacts();
+            }
+        } else {
+            getContacts();
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode) {
+            case PERMISSIONS_REQUEST_READ_CONTACTS: {
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    getContacts();
+                } else {
+                    Toast.makeText(this, "You have disabled a contacts permission", Toast.LENGTH_LONG).show();
+                }
+                return;
+            }
+        }
+    }
+    private ArrayList<String> getnumberlist(ArrayList<Contact> contactlist){
+        ArrayList<String> numberlist = new ArrayList<String>();
+        if(contactlist.isEmpty()){
+            numberlist.add("01012345678");
+        }
+        else{
+            for(int i = 0; i < contactlist.size(); i++){
+                numberlist.add(contactlist.get(i).getPhonenum());
+            }
+        }
+        return numberlist;
+    }
+
+    //TAB2//
+
+    //TAB3//
+
+    //TAB//
     private void changeView(int index) {
-        LinearLayout linearLayout1 = (LinearLayout) findViewById(R.id.LinearLayout1) ;
-        TextView textView1 = (TextView) findViewById(R.id.text1) ;
-        TextView textView2 = (TextView) findViewById(R.id.text2) ;
-        TextView textView3 = (TextView) findViewById(R.id.text3) ;
+        LinearLayout TAB1 = (LinearLayout) findViewById(R.id.TAB1) ;
 
         switch (index) {
             case 0 :
-                linearLayout1.setVisibility(View.VISIBLE);
-                textView1.setVisibility(View.INVISIBLE) ;
-                textView2.setVisibility(View.INVISIBLE) ;
-                textView3.setVisibility(View.INVISIBLE) ;
+                TAB1.setVisibility(View.VISIBLE);
                 break ;
             case 1 :
-                linearLayout1.setVisibility(View.INVISIBLE);
-                textView1.setVisibility(View.INVISIBLE) ;
-                textView2.setVisibility(View.VISIBLE) ;
-                textView3.setVisibility(View.INVISIBLE) ;
+                TAB1.setVisibility(View.INVISIBLE);
                 break ;
             case 2 :
-                linearLayout1.setVisibility(View.INVISIBLE);
-                textView1.setVisibility(View.INVISIBLE) ;
-                textView2.setVisibility(View.INVISIBLE) ;
-                textView3.setVisibility(View.VISIBLE) ;
+                TAB1.setVisibility(View.INVISIBLE);
                 break ;
 
         }
